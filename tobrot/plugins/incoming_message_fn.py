28651110b5bd1cfd396890ca4ec65bbaae00b267
HappyBoy0509 <65845452+HappyBoy0509@users.noreply.github.com>
2020-07-07 21:18:46 +0530

    Update incoming_message_fn.py

--- tobrot/plugins/incoming_message_fn.py
+++ tobrot/plugins/incoming_message_fn.py
@@ -42,7 +42,7 @@ async def incoming_purge_message_f(client, message):
 
 async def incoming_message_f(client, message):
     """/leech command"""
-    i_m_sefg = await message.reply_text("processing", quote=True)
+    i_m_sefg = await message.reply_text("Processing", quote=True)
     is_zip = False
     if len(message.command) > 1:
         if message.command[1] == "archive":
@@ -52,7 +52,7 @@ async def incoming_message_f(client, message):
     LOGGER.info(dl_url)
     LOGGER.info(cf_name)
     if dl_url is not None:
-        await i_m_sefg.edit_text("extracting links")
+        await i_m_sefg.edit_text("Extracting Links")
         # start the aria2c daemon
         aria_i_p = await aria_start()
         LOGGER.info(aria_i_p)
@@ -66,7 +66,7 @@ async def incoming_message_f(client, message):
         # create download directory, if not exist
         if not os.path.isdir(new_download_location):
             os.makedirs(new_download_location)
-        await i_m_sefg.edit_text("trying to download")
+        await i_m_sefg.edit_text("Trying 2 download")
         # try to download the "link"
         sagtus, err_message = await call_apropriate_function(
             aria_i_p,
@@ -80,7 +80,7 @@ async def incoming_message_f(client, message):
             await i_m_sefg.edit_text(err_message)
     else:
         await i_m_sefg.edit_text(
-            "**FCUK**! wat have you entered. \nPlease read /help \n"
+            "**Fuck**! What have you entered. \nReply To Magnet Link R Direct Link\n Read Pinned Message\n"
             f"<b>API Error</b>: {cf_name}"
         )
 
@@ -126,6 +126,6 @@ async def incoming_youtube_dl_f(client, message):
             )
     else:
         await i_m_sefg.edit_text(
-            "**FCUK**! wat have you entered. \nPlease read /help \n"
+            "**Fuck**! What have you entered. \nReply To Magnet Link R Direct Link\n Read Pinned Message\n"
             f"<b>API Error</b>: {cf_name}"
         )
