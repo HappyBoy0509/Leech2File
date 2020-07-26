f18b3fde3d0260ca64a0e819065979dabf1b02df
HappyBoy0509 <65845452+HappyBoy0509@users.noreply.github.com>
2020-07-26 09:44:36 +0530

    Update new_join_fn.py

--- tobrot/plugins/new_join_fn.py
+++ tobrot/plugins/new_join_fn.py
@@ -47,13 +47,13 @@ async def rename_message_f(client, message):
     inline_keyboard = []
     inline_keyboard.append([
         pyrogram.InlineKeyboardButton(
-            text="read this?",
-            url="https://t.me/keralagram/698909"
+            text="More",
+            url="https://t.me/hb4all_bot"
         )
     ])
     reply_markup = pyrogram.InlineKeyboardMarkup(inline_keyboard)
     await message.reply_text(
-        "please use @renamebot",
+        "please use @rename_ro_bot",
         quote=True,
         reply_markup=reply_markup
     )
